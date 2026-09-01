48a5012833a514872fa32448fc275115b2473e0d
Myck <136953823+Kmykh@users.noreply.github.com>
2026-09-01 12:05:08 -0500

    style: añadir imágenes y eliminar contenedor de la pantalla de mantenimiento

--- src/components/shared/MaintenanceScreen.tsx
+++ src/components/shared/MaintenanceScreen.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './MaintenanceScreen.module.css';
+import flo1 from '@/app/(public)/images/flo1.png';
+import flo2 from '@/app/(public)/images/flo2.png';
+import angeles from '@/app/(public)/images/angeles.png';
 
 export default function MaintenanceScreen() {
   return (
     <div className={styles.container}>
+      {/* Elementos decorativos */}
+      <div className={styles.decoTopRight}>
+        <Image src={flo1} alt="" width={300} height={300} className={styles.floatingImage} />
+      </div>
+      <div className={styles.decoBottomLeft}>
+        <Image src={flo2} alt="" width={250} height={250} className={styles.floatingImageAlt} />
+      </div>
+      <div className={styles.decoCenter}>
+        <Image src={angeles} alt="" width={400} height={400} className={styles.subtleImage} />
+      </div>
+
       <div className={styles.content}>
         <h4 className={styles.kicker}>PRÓXIMAMENTE</h4>
         <h1 className={styles.title}>Nuevas creaciones en camino</h1>
